a3c3b28d92d705c9a13fdeba9af0be23494ae76a
Sebastian Jensen <111466887+HEXtriple@users.noreply.github.com>
2023-05-21 16:29:28 +0200

    extreme edits

---
+++ js/cultButton.js
@@ -0,0 +1,8 @@
+let heroImage = document.getElementsByClassName(".hero-image");
+
+let button = document.getElementById("join-the-cult");
+
+button.addEventListener("click", function() {
+  //heroImage.style.backgroundImage = "url('../images/Danderyd.jpg')";
+  alert("You have joined the cult!");
+});
